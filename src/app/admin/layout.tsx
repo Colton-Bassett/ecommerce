18d19e9c07af6527828b37890e5bcdb55925db8c
Colton Bassett <coltonbassett@gmail.com>
2024-10-22 14:22:52 -0700

    Added turbopack and shadcn. Started work on adminDashboard and nav

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,19 @@
+import { Nav, NavLink } from "../components/nav";
+
+export default function AdminLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<>
+			<Nav>
+				<NavLink href="/admin">Dashboard</NavLink>
+				<NavLink href="/admin/products">Products</NavLink>
+				<NavLink href="/admin/users">Customers</NavLink>
+				<NavLink href="/admin/sales">Sales</NavLink>
+			</Nav>
+			<div className="container m-6">{children}</div>
+		</>
+	);
+}
